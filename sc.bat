57713bf44cca949d24baa09c2939109a2fad4598
Neon22 <mschafer@wireframe.biz>
2020-04-02 22:45:17 +1300

    adding sc replacements

---
+++ sc.bat
@@ -0,0 +1,12 @@
+@echo off
+set foo=sc
+@echo on
+@echo "Renaming lines in %foo%.xml to configuration folder"
+@echo off
+call far.bat ./news "%localappdata%/Stitch Works Software/Crochet Charts/news" %foo%.xml>temp.xml
+@echo on
+echo "processed"
+del "%foo%.xml"
+echo "Original deleted"
+rename "temp.xml" "%foo%.xml"
+echo "Renamed and done. !"
